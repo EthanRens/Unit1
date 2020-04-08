86af68f776febba57eab7c153ebe361b6e252a77
EthanRens <815303@mystma.org>
2020-04-08 15:48:39 +0000

    Added tio five movies

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -2,6 +2,15 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+        System.out.println("1. Wall-E");
+        System.out.println("2. Click");
+        System.out.println("3. Killer Bean");
+        System.out.println("4. Foodfight");
+        System.out.println("5. Little Rascals");
+        System.out.println("6. Diary of a Wimpy Kid: The Movie");
+        System.out.println("7. Joshua and The Promised Land");
+        System.out.println("8. The Lego Movie");
+        System.out.println("9. Monsters vs Aliens");
+        System.out.println("10. Gnomeo and Juliet");
     }
 }
