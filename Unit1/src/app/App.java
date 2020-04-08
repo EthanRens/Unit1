6889de019850ad8f5a99ccb680497da73218595b
EthanRens <815303@mystma.org>
2020-04-08 15:49:52 +0000

    Added top five movies [Fixed]

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -7,10 +7,5 @@ public class App {
         System.out.println("3. Killer Bean");
         System.out.println("4. Foodfight");
         System.out.println("5. Little Rascals");
-        System.out.println("6. Diary of a Wimpy Kid: The Movie");
-        System.out.println("7. Joshua and The Promised Land");
-        System.out.println("8. The Lego Movie");
-        System.out.println("9. Monsters vs Aliens");
-        System.out.println("10. Gnomeo and Juliet");
     }
 }
