7da51f701e0851e042482012b737cb9aa3208091
EthanRens <815303@mystma.org>
2020-04-13 15:17:54 +0000

    Finished Mad Lib

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -2,36 +2,20 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        int myFavNum = 46293;
+        String person2 = "Isaac";
+        String food = "Pizza";
+        String number = "25";
+        String adjective1 = "Tall";
+        String adjective2 = "Short";
+        String nationality = "German";
+        String noun1 = "Pizza dough";
+        String noun2 = "Box";
 
-        System.out.println(myFavNum);
-
-        boolean livesInAlbertville = false;
-
-        System.out.println(livesInAlbertville);
-
-        byte amtOfCookie = 12;
-
-        System.out.println(amtOfCookie);
-
-        char favLetter = 'R';
-
-        System.out.println(favLetter);
-
-        short amtOfPets = 24333;
-
-        System.out.println(amtOfPets);
-
-        long worldPopulation = 70000000;
-
-        System.out.println(worldPopulation);
-
-        float amtOfSocks = 5;
-
-        System.out.println(amtOfSocks);
-
-        double smallNumber = 0.35633;
-
-        System.out.println(smallNumber);
+        System.out.println("Pizza was invted by a " + adjective1 + " " + nationality + " chef named " + person2);
+        System.out.println("To make a pizza, you need to take a lump of " + noun1 + ", and make a thin, round " + adjective2 + " " + noun2);
+        System.out.println("Then you cover it with " + adjective1 + " sauce, " + adjective2 + " cheese, and fresh chopped " + noun1);
+        System.out.println("Next you have to bake it in a very hot " + noun2);
+        System.out.println("Some kids like " + food + " pizza the best, but my favirote is the " + food + " pizza");
+        System.out.println("If I could, I would eat pizza " + number + " times a day!" );
     }
 }
