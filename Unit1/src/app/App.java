013e61f6c28201d658eec81284d9e77ae06015e0
EthanRens <815303@mystma.org>
2020-04-14 15:53:42 +0000

    While systems finished

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -2,20 +2,25 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        String person2 = "Isaac";
-        String food = "Pizza";
-        String number = "25";
-        String adjective1 = "Tall";
-        String adjective2 = "Short";
-        String nationality = "German";
-        String noun1 = "Pizza dough";
-        String noun2 = "Box";
+        int count = 0;
+        String color = "red";
+        boolean winter = false;
 
-        System.out.println("Pizza was invted by a " + adjective1 + " " + nationality + " chef named " + person2);
-        System.out.println("To make a pizza, you need to take a lump of " + noun1 + ", and make a thin, round " + adjective2 + " " + noun2);
-        System.out.println("Then you cover it with " + adjective1 + " sauce, " + adjective2 + " cheese, and fresh chopped " + noun1);
-        System.out.println("Next you have to bake it in a very hot " + noun2);
-        System.out.println("Some kids like " + food + " pizza the best, but my favirote is the " + food + " pizza");
-        System.out.println("If I could, I would eat pizza " + number + " times a day!" );
+        while (count < 10) {
+            System.out.println("The count is " + count);
+            count = count + 1;
+        }
+
+        while (color == "red") {
+            System.out.println("The color is " + color);
+            color = "green";
+        }
+
+        while (winter = false) {
+            System.out.println("Is it winter? " + winter);
+            winter = true;
+        }
+        System.out.println("count finished");
+        System.out.println("It's Winter");
     }
 }
