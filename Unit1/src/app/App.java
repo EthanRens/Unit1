2e7c848df22805be3fa51d5b99a5e279996396d1
EthanRens <815303@mystma.org>
2020-04-15 15:48:47 +0000

    finished while statements

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -2,25 +2,32 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        int count = 0;
-        String color = "red";
-        boolean winter = false;
 
-        while (count < 10) {
-            System.out.println("The count is " + count);
-            count = count + 1;
+        for (int i = 0; i <= 10; i = i +1) {
+            System.out.println("The count is " + i);
+            System.out.println("Let's keep counting.");
         }
-
-        while (color == "red") {
-            System.out.println("The color is " + color);
-            color = "green";
+        for (int f = 10; f <= 20; f = f +1) {
+            System.out.println("The count is " + f);
+            System.out.println("Let's keep counting.");
         }
-
-        while (winter = false) {
-            System.out.println("Is it winter? " + winter);
-            winter = true;
+        for (int r = 10; r <= 100; r = r +1) {
+            System.out.println("The count is " + r);
+            System.out.println("Let's keep counting.");
         }
-        System.out.println("count finished");
-        System.out.println("It's Winter");
+        for (int c = 1000; c <= 256000; c = c +1) {
+            System.out.println("The count is " + c);
+            System.out.println("Let's keep counting.");
+        }
+        for (int b = 11; b <= 12; b = b +1) {
+            System.out.println("The count is " + b);
+            System.out.println("Let's keep counting.");
+        }
+        for (int t = 10; t <= 0; t = t -1) {
+            System.out.println("The count is " + t);
+            System.out.println("Let's keep counting.");
+        }
     }
 }
+        
+    
