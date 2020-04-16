b6c5602870dad05a85d91dda08a32cb30646fc4e
EthanRens <815303@mystma.org>
2020-04-16 15:59:39 +0000

    taking a break I'm stuck

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -3,25 +3,21 @@ package app;
 public class App {
     public static void main(String[] args) throws Exception {
 
-        for (int i = 0; i <= 10; i = i +1) {
-            System.out.println("The count is " + i);
-            System.out.println("Let's keep counting.");
+        for (int i = 22; i <= 28; i = i +3) {
+            System.out.println(i);
         }
-        for (int f = 10; f <= 20; f = f +1) {
-            System.out.println("The count is " + f);
-            System.out.println("Let's keep counting.");
+        for (int f = 2; f <= 32; f = f *2) {
+            System.out.println(f);
         }
-        for (int r = 10; r <= 100; r = r +1) {
-            System.out.println("The count is " + r);
-            System.out.println("Let's keep counting.");
+        for (int r = 3; r >= -81; r = r *-r) {
+            System.out.println(r);
         }
-        for (int c = 1000; c <= 256000; c = c +1) {
-            System.out.println("The count is " + c);
-            System.out.println("Let's keep counting.");
+        for (int c = 50; c >= 5; c = c -5) {
+            System.out.println(c);
         }
         for (int b = 11; b <= 12; b = b +1) {
             System.out.println("The count is " + b);
-            System.out.println("Let's keep counting.");
+            System.out.println();
         }
         for (int t = 10; t <= 0; t = t -1) {
             System.out.println("The count is " + t);
