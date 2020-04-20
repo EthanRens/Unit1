8855105c9117dcf4f44747b55a5f65e380884831
EthanRens <815303@mystma.org>
2020-04-20 14:47:30 +0000

    Finished if statements

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -3,25 +3,22 @@ package app;
 public class App {
     public static void main(String[] args) throws Exception {
 
-        for (int i = 22; i <= 28; i = i +3) {
-            System.out.println(i);
+        for (int i = 1; i <= 20; i++) {
+            if (i % 4 == 0) {
+                System.out.println(i + " is divisible by 4");
+            } else {
+                System.out.println(i + " is not divisible by 4");
+            }
         }
-        for (int f = 2; f <= 32; f = f *2) {
-            System.out.println(f);
-        }
-        for (int r = 3; r >= -81; r = r *-r) {
-            System.out.println(r);
-        }
-        for (int c = 50; c >= 5; c = c -5) {
-            System.out.println(c);
-        }
-        for (int b = 1; b <= 9; b = b +1) {
-            System.out.println(b * b);
-            System.out.println(b + 3);
-            System.out.println(b);
-        }
+        System.out.println("All finished");
 
+        for (int i = 1; i <= 10; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i + " is and even number");
+            } else {
+                System.out.println(i + " is an odd number");
+            }
+        }
+        System.out.println("All finished");
     }
 }
-        
-    
