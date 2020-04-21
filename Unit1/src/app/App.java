1b4706c3c66a60112404d326686bdf4f50b0f9c9
EthanRens <815303@mystma.org>
2020-04-21 15:51:12 +0000

    Taking a break

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -3,22 +3,21 @@ package app;
 public class App {
     public static void main(String[] args) throws Exception {
 
-        for (int i = 1; i <= 20; i++) {
-            if (i % 4 == 0) {
-                System.out.println(i + " is divisible by 4");
-            } else {
-                System.out.println(i + " is not divisible by 4");
-            }
-        }
-        System.out.println("All finished");
+        for (int i = 0; i <= 19; i++) {
+            System.out.println(i);
+            if (i == 10) { 
+                for (int e = 0; e <= 4; e++) {
+                    System.out.println(10);
+                } 
+            }}
+        for (int f = -1; f <= 17; f++) {
+            System.out.println(f);
+            if (f >= 20){ 
+                
+                System.out.println(f + 1);
+            }}
+        } 
 
-        for (int i = 1; i <= 10; i++) {
-            if (i % 2 == 0) {
-                System.out.println(i + " is and even number");
-            } else {
-                System.out.println(i + " is an odd number");
-            }
         }
-        System.out.println("All finished");
     }
 }
