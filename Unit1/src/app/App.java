ee531a329419e53fac32c3d33d87d51c9f198277
EthanRens <815303@mystma.org>
2020-04-21 19:20:45 +0000

    I couldn't figure out the second loop

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -3,21 +3,21 @@ package app;
 public class App {
     public static void main(String[] args) throws Exception {
 
-        for (int i = 0; i <= 19; i++) {
+for (int i = 0; i <= 19; i++) {
             System.out.println(i);
             if (i == 10) { 
                 for (int e = 0; e <= 4; e++) {
                     System.out.println(10);
                 } 
             }}
-        for (int f = -1; f <= 17; f++) {
-            System.out.println(f);
-            if (f >= 20){ 
-                
-                System.out.println(f + 1);
+ for (int f = -1; f <= 19; f++) {
+            if (f % 2 == 0){
+                System.out.println(f + 3); 
+            }else{
+                System.out.println(f + 2);
             }}
         } 
 
         }
-    }
-}
+    
+
