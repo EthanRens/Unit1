8f286d606f340913cac9779792c06af4d920a492
EthanRens <815303@mystma.org>
2020-04-22 16:09:19 +0000

    Finished the logic challenge

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -3,21 +3,22 @@ package app;
 public class App {
     public static void main(String[] args) throws Exception {
 
-for (int i = 0; i <= 19; i++) {
+for (int i = 20; i >= 1; i--) {
             System.out.println(i);
             if (i == 10) { 
                 for (int e = 0; e <= 4; e++) {
                     System.out.println(10);
                 } 
             }}
- for (int f = -1; f <= 19; f++) {
-            if (f % 2 == 0){
-                System.out.println(f + 3); 
-            }else{
-                System.out.println(f + 2);
+ for (int f = 0; f <= 9; f++) {
+     System.out.println(f * 4);
+            if (f % 3 == 0){
+                System.out.println("Hey!"); 
+            }
+                
             }}
         } 
 
-        }
+        
     
 
