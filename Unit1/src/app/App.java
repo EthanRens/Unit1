80e6e863d222e236257378315124ac11ccf356e5
EthanRens <815303@mystma.org>
2020-04-22 16:26:12 +0000

    Finished the logic challenge

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -2,21 +2,22 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-
-for (int i = 20; i >= 1; i--) {
+System.out.println("Start First Loop");
+for (int i = 20; i >= 0; i--) {
             System.out.println(i);
             if (i == 10) { 
                 for (int e = 0; e <= 4; e++) {
                     System.out.println(10);
                 } 
-            }}
+            }}System.out.println("End first loop");
+            System.out.println("Start Second Loop");
  for (int f = 0; f <= 9; f++) {
      System.out.println(f * 4);
             if (f % 3 == 0){
                 System.out.println("Hey!"); 
             }
                 
-            }}
+            }System.out.println("End second loop");}
         } 
 
         
