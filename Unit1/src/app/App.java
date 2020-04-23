a4eaa9f7218109c3ad2b962668bbd23b70e8b755
EthanRens <815303@mystma.org>
2020-04-23 16:16:11 +0000

    Finished the project for today

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -2,24 +2,32 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-System.out.println("Start First Loop");
-for (int i = 20; i >= 0; i--) {
-            System.out.println(i);
-            if (i == 10) { 
-                for (int e = 0; e <= 4; e++) {
-                    System.out.println(10);
-                } 
-            }}System.out.println("End first loop");
-            System.out.println("Start Second Loop");
- for (int f = 0; f <= 9; f++) {
-     System.out.println(f * 4);
-            if (f % 3 == 0){
-                System.out.println("Hey!"); 
-            }
-                
-            }System.out.println("End second loop");}
+        System.out.println("Double or single digit?");
+        for (int g = 20; g >=1; g--){
+            if (g >= 10){
+                System.out.println(g + " is a double digit");
+            
+            }else{
+                System.out.println(g + " is a single digit");
+            }}
+        
+            System.out.println("Nice!");
+            System.out.println("Fast food");
+        for (int f = 1; f <= 10; f++){
+        if (f % 2 == 0){
+                System.out.println("Fries");
+           }
+           else{
+               System.out.println("Burgers");
+           }
         } 
+        System.out.println("Nice!"); 
+    }
 
+
+
+
+}
         
     
 
