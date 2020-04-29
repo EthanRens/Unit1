1cc18a5807d1a8a8db5d2346c0cbd760858e7030
EthanRens <815303@mystma.org>
2020-04-29 15:27:12 +0000

    finished the challenge

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -6,18 +6,15 @@ public class App {
         
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Please enter your first name.");
-        String firstName = input.nextLine();
-        System.out.println("Please enter your last name.");
-        String lastName = input.nextLine();
-        System.out.println("Please enter your age.");
-        String age = input.nextLine();
-        System.out.println("Please enter your grade number.");
-        String grade = input.nextLine();
-        System.out.println("Please enter your favirote color.");
-        String favColor = input.nextLine();
+        System.out.println("Please enter the password.");
+        String password = input.nextLine();
 
-        System.out.println("Hello, " + firstName + " " + lastName + ". You are " + age + " years old, you are in " + grade + "th grade, and your favirote color is " + favColor + ".");
+
+        if (password.equals("183726073")){
+            System.out.println("ACCESS GRANTED");
+        } else {
+            System.out.println("ACCESS DENIED");
+        }
 
 
     }
