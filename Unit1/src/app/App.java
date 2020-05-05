fc3ec04ea53fa2ef07760d39bff42141e80d7af6
EthanRens <815303@mystma.org>
2020-05-05 14:11:39 +0000

    finished the quiz

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -3,21 +3,48 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        
+
         Scanner input = new Scanner(System.in);
 
-              System.out.println("What is the count");
-        
-         int number = input.nextInt();
+        System.out.println("How many inches are in a foot?");
+        String password = input.nextLine();
 
-     for (int q = 1; q <= number; q ++){
-               System.out.println(q);
+
+        if (password.equals("12")){
+            System.out.println("CORRECT");
+        } else {
+            System.out.println("INCORRECT");
         }
 
 
-    }
-}
 
+            System.out.println("What is the capitol of Minnesota?");
+        String password2 = input.nextLine();
+
+
+        if (password2.equals("St. Paul")){
+            System.out.println("CORRECT");
+        } else {
+            System.out.println("INCORRECT");
+        }
+
+
+
+
+
+                System.out.println("What year is it?");
+        String password3 = input.nextLine();
+
+
+        if (password3.equals("2020")){
+            System.out.println("CORRECT");
+        } else {
+            System.out.println("INCORRECT");
+        }
+    
+    
+    
+    }}
 
 
         
