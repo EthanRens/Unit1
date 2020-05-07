e3f2d67374efb5b9e95a3fcc67dd7a77a6059061
EthanRens <815303@mystma.org>
2020-05-07 14:10:22 +0000

    Finished the mad lib

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -6,44 +6,34 @@ public class App {
 
         Scanner input = new Scanner(System.in);
 
-        System.out.println("How many inches are in a foot?");
-        String password = input.nextLine();
+        System.out.println("Please enter a noun.");
+        String noun1 = input.nextLine();
 
-
-        if (password.equals("12")){
-            System.out.println("CORRECT");
-        } else {
-            System.out.println("INCORRECT");
-        }
-
-
-
-            System.out.println("What is the capitol of Minnesota?");
-        String password2 = input.nextLine();
-
-
-        if (password2.equals("St. Paul")){
-            System.out.println("CORRECT");
-        } else {
-            System.out.println("INCORRECT");
-        }
-
-
-
-
-
-                System.out.println("What year is it?");
-        String password3 = input.nextLine();
-
-
-        if (password3.equals("2020")){
-            System.out.println("CORRECT");
-        } else {
-            System.out.println("INCORRECT");
-        }
-    
-    
-    
+        System.out.println("Please enter a name.");
+        String name1 = input.nextLine();  
+        
+        System.out.println("Please enter a verb.");
+        String verb1 = input.nextLine();    
+        
+        System.out.println("Please enter an adjective.");
+        String adj1 = input.nextLine();            
+  
+        System.out.println("Please enter a place.");
+        String place1 = input.nextLine();      
+        
+        System.out.println("Please enter a food.");
+        String food1 = input.nextLine();   
+        
+        System.out.println("Please enter an animal.");
+        String anim1 = input.nextLine();  
+        
+        System.out.println("Please enter a number.");
+        String num1 = input.nextLine();  
+        
+        System.out.println("This christmas I really want to get a " + noun1 + ", but my parent, " + name1+ ", would never let me get it.");
+        System.out.println("If I were to get it, I would " + verb1 + " straight to my " + adj1 + " friend's house and show them.");
+        System.out.println("After that we would go to " + place1 + " to eat " + food1 + ".");
+        System.out.println("I heard that they might have a " + anim1 + " there that's almost " + num1 + " years old!");
     }}
 
 
