4b1135e79e0382db75496db6b446f26f279a5ca8
EthanRens <815303@mystma.org>
2020-05-11 15:08:17 +0000

    taking a break

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -6,35 +6,30 @@ public class App {
 
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Please enter a noun.");
-        String noun1 = input.nextLine();
+        System.out.println("Type a number");
+        int num1 = input.nextInt();
 
-        System.out.println("Please enter a name.");
-        String name1 = input.nextLine();  
-        
-        System.out.println("Please enter a verb.");
-        String verb1 = input.nextLine();    
-        
-        System.out.println("Please enter an adjective.");
-        String adj1 = input.nextLine();            
-  
-        System.out.println("Please enter a place.");
-        String place1 = input.nextLine();      
-        
-        System.out.println("Please enter a food.");
-        String food1 = input.nextLine();   
-        
-        System.out.println("Please enter an animal.");
-        String anim1 = input.nextLine();  
-        
-        System.out.println("Please enter a number.");
-        String num1 = input.nextLine();  
-        
-        System.out.println("This christmas I really want to get a " + noun1 + ", but my parent, " + name1+ ", would never let me get it.");
-        System.out.println("If I were to get it, I would " + verb1 + " straight to my " + adj1 + " friend's house and show them.");
-        System.out.println("After that we would go to " + place1 + " to eat " + food1 + ".");
-        System.out.println("I heard that they might have a " + anim1 + " there that's almost " + num1 + " years old!");
-    }}
+        System.out.println("Type a second number");
+        int num2 = input.nextInt();
+
+        System.out.println("Enter a function");
+        String function = input.nextLine();
+
+        if (function.equals("+")){
+            System.out.println(num1 + num2);
+        }
+
+        if (function.equals("*"))    {
+            System.out.println(num1 * num2);
+        }
+
+        if (function.equals("-")){
+            System.out.println(num1 - num2);
+                }
+
+        if (function.equals("/")){
+         System.out.println(num1 / num2);
+        }}}
 
 
         
